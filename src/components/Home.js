a7c3b18261535d1e02c2ccb753c8cdc2bd6fe3a3
artur893 <arthal893@gmail.com>
2022-08-29 13:18:14 +0200

    Initial commit, landing page, link to store

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='center'>
+        <h1 className='title'>Your fish online store</h1>
+       <a href='/products' className='visit'>Visit now</a>
+      </div>
+
+    </div>
+  );
+};
+
+export default Home;
